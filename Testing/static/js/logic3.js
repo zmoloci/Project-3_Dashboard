e6d620215ae44076e8fe316697f0d6ef6f1b1f82
zmoloci <120351966+zmoloci@users.noreply.github.com>
2023-04-21 18:16:18 -0400

    Add files via upload

--- Testing/static/js/logic3.js
+++ Testing/static/js/logic3.js
@@ -1,3 +1,12 @@
+// Transforming WGS84 Projected Coordinates to Lat/Long:
+// From: https://javascript.plainenglish.io/transforming-xy-coordinates-to-latlng-values-with-proj4-js-d91a6139ba90
+
+var fromProj = proj4.defs('EPSG:42105');
+var toProj = proj4.defs('EPSG:4326');
+
+// Leaflet conversion unproject:
+var point
+
 // Creating the map object
 var myMap = L.map("map", {
   center: [49.246292, -123.116226],
@@ -15,17 +24,27 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // USE THIS LINK TO TEST without broken West Van Data:
 var link = "https://zmoloci.github.io/Project-3_Dashboard/cleaned_CMHC_ZONE-noWestVan.geojson";
 
+// USE THIS LINK TO TEST pre-converted coordinates and no West Van:
+// var link = "https://zmoloci.github.io/Project-3_Dashboard/cleaned_CMHC_ZONE-noWestVan-latlong";
+
+
 // Getting our GeoJSON data
 d3.json(link).then(function (data) {
   // Convert coordinates to Lat/Long
   for (var i = 0; i < data.length; i++) {
     if (i == 16) { i++ };
     for (var j = 0; j < data[i].geometry.coordinates[0].length; j++) {
-      // data[i].geometry.coordinates[0][j][0] = (((data[i].geometry.coordinates[0][j][1]) / 20037508.34) * 180)
-      // data[i].geometry.coordinates[0][j][1] = (((data[i].geometry.coordinates[0][j][0]) / 20037508.34) * 180)
-      data[i].geometry.coordinates[0][j] = [(((data[i].geometry.coordinates[0][j][1]) / 20037508.34) * 180), (((data[i].geometry.coordinates[0][j][0]) / 20037508.34) * 180)]
+      console.log(data[i].geometry.coordinates[0][0])
+      console.log(data[i].geometry.coordinates[0])
+      //leaflet.js unproject attempt (EPSG42105 to EPSG4326):
+      point - L.CRS.EPSG42105.unproject(L.) data[i].geometry.coordinates[0][j] =
+
+        //proj4.js attempt:
+        //data[i].geometry.coordinates[0][j] = proj4(fromProj, toProj, [data[i].geometry.coordinates[0][j][0], data[i].geometry.coordinates[0][j][1]])
+
+        console.log(data[i].geometry.coordinates[0][j])
     };
-    // console.log(data[i]);
+    console.log(data[i]);
     // L.geoJson(data, {
     //   style: function (feature) {
     //     return {
